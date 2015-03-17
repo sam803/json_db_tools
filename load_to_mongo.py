f3ee0938ed9b14182fde356c376b7766dc00af34
Sam Barker <samuel.j.w.barker@gmail.com>
2015-03-17 12:59:08 +0000

    _id is md5 hash

--- load_to_mongo.py
+++ load_to_mongo.py
@@ -9,7 +9,20 @@ import tarfile
 import gzip
 import zlib
 import uuid
+import md5
 
+def load(filestr):
+  a = json.loads(filestr)
+  for d in a: 
+    m = md5.new()
+    keys = d.keys()
+    keys.sort()
+    for key in keys:
+      v = d[key]
+      if v != None:
+        m.update(str(v))
+    d['_id'] = m.hexdigest()
+  return a
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
@@ -21,20 +34,20 @@ if __name__ == "__main__":
   
   client = pymongo.MongoClient('localhost')
   tmpDir = utils.uncompress(fpath)
-  print tmpDir
   for f in os.listdir(tmpDir):
-    print f + ' ' +os.path.join(tmpDir, f)
     with open(os.path.join(tmpDir, f), 'r') as the_file:
       try:
-        toinsert = json.loads(the_file.read()) 
+        toinsert = load(the_file.read()) 
         #todo create hash of record and use as _id so no duplicates can be created
         if len(toinsert) > 0:
           d,c = utils.parseFileName(f)
           client[d][c].insert(toinsert)
           print 'created ' + str(len(toinsert)) + ' records in ' + d + '.' + c
       except ValueError:
-         print 'could not decode JSON object from ' + f
-
+        print 'could not decode JSON object from ' + f
+      except pymongo.errors.DuplicateKeyError:
+   	#already exists
+	pass
   utils.removeTmpDir(tmpDir) 
   
    
